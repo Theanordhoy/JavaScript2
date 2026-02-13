ee5d5aaf549dad0b7addaef5e51ce3ef55139225
Emilie Sofie Fosmo <emilie.fosmo@hotmail.com>
2026-02-13 14:35:15 +0100

    Added comment, reply and react.

---
+++ js/api/comments.js
@@ -0,0 +1,70 @@
+const BASE_URL = "https://v2.api.noroff.dev/social";
+const API_KEY = "143a7d73-1893-43f4-bca0-8f81d8e2443f";
+
+function getHeaders() {
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) {
+        throw new Error("No access token found.");
+    }
+
+    return {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json"
+    };
+}
+
+//Create commment or reply to comment
+export async function createComment(postId, body, replyToId = null) {
+    const payload = { body: body };
+
+    if (replyToId !== null) {
+        payload.replyToId = Number(replyToId);
+    }
+
+    const response = await fetch(
+        `${BASE_URL}/posts/${postId}/comment`,
+        {
+            method: "POST",
+            headers: getHeaders(),
+            body: JSON.stringify(payload)
+        }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.errors?.[0]?.message || "Failed to create comment.");
+    }
+
+    return data.data;
+}
+
+
+//React to a post
+export async function reactToPost(postId) {
+
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) {
+        throw new Error("No access token found.")
+    }
+
+    const response = await fetch(
+        `${BASE_URL}/posts/${postId}/react/👍`,
+        {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "X-Noroff-API-Key": API_KEY
+            } 
+        }
+    );
+
+    if (!response.ok) {
+       throw new Error(`Reaction failed: ${response.status}`);
+    }
+
+    return true;
+};
--- js/main/post.js
+++ js/main/post.js
@@ -1,4 +1,5 @@
 import { getSinglePost } from "../api/posts.js";
+import { createComment, reactToPost } from "../api/comments.js";
 import { renderSinglePost } from "../ui/renderPost.js";
 
 const container = document.getElementById("singlePostContainer");
@@ -15,14 +16,119 @@ async function fetchSinglePost() {
         }
 
         const post = await getSinglePost(postId);
+
         renderSinglePost(post, container);
-    
+        attachReactionListener();
+        attachCommentListener();
+        attachReplyListener();
+
     } catch (error) {
         console.error("Error fetching post:", error);
         container.innerHTML = "<p class='error'>Error loading post.</p>";
     }
 }
 
+//Attach reaction after rendering
+function attachReactionListener() {
+    const reactionButton = document.querySelector(".reaction-button");
 
+    if (!reactionButton) return;
+
+    reactionButton.addEventListener("click", async () => {
+        try {
+            await reactToPost(postId, "👍");
+
+            fetchSinglePost();
+
+        } catch (error) {
+            console.error("Reaction failed:", error);
+        }
+    });
+}
+
+//Comments
+function attachCommentListener() {
+    const form = document.querySelector(".comment-form");
+
+    if (!form) return;
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const textarea = form.querySelector("textarea");
+        const body = textarea.value.trim();
+
+        
+        if (!body) return;
+
+        try {
+            await createComment(postId, body);
+            textarea.value = "";
+            await fetchSinglePost();
+        } catch (error) {
+            console.error("Comment failed:", error);
+        }
+    })
+}
+
+function attachReplyListener() {
+    const replyButtons = document.querySelectorAll(".reply-button");
+
+    replyButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            
+            const commentItem = button.closest(".comment-item");
+            //If replyform exists already - remove 
+            const existingForm = commentItem.querySelector(".reply-form");
+            if (existingForm) {
+                existingForm.remove();
+                return;
+            }
+
+            const replyForm = document.createElement("form");
+            replyForm.className = "reply-form";
+
+            const label = document.createElement("label");
+            label.textContent = "Write a reply";
+            label.setAttribute("for", "textareaId");
+            label.className = "sr-only";
+
+            const textarea = document.createElement("textarea");
+            textarea.required = true;
+            textarea.placeholder = "Write a reply";
+            textarea.className = "comment-input";
+            textarea.id = "textareaId";
+
+
+
+            const submitBtn = document.createElement("button");
+            submitBtn.type = "submit";
+            submitBtn.textContent = "Reply";
+            submitBtn.className = "comment-button";
+
+            replyForm.appendChild(textarea);
+            replyForm.appendChild(submitBtn);
+
+            commentItem.appendChild(replyForm);
+
+            //Submit reply
+            replyForm.addEventListener("submit", async (e) => {
+                e.preventDefault();
+
+                const body = textarea.value.trim();
+                if (!body) return;
+
+                const replyToId = commentItem.dataset.commentId;
+
+                try {
+                    await createComment(postId, body, replyToId);
+                    await fetchSinglePost();
+                } catch (error) {
+                    console.error("Reply failed:", error)
+                }
+            })
+        })
+    })
+}
 fetchSinglePost();
 
